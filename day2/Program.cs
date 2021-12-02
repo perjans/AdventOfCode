d07f7f97cc877790589f90b81283e261a9e850f2
perjans <70916077+perjans@users.noreply.github.com>
2021-12-02 09:46:52 +0100

    Add files via upload

---
+++ day2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace day2 {
+    class Program {
+        static void Main(string[] args) {
+            string text = System.IO.File.ReadAllText(@"/Users/perjansson/Projects/day2/day2/day2.txt");
+            string[] inputs = text.Split('\n');
+
+            int horizontal = 0;
+            int depth = 0;
+
+            //Part 1
+
+            foreach (var input in inputs) {
+                string direction = input.Split(' ')[0];
+                int value = int.Parse(input.Split(' ')[1]);
+
+                switch (direction) {
+                    case "forward":
+                        horizontal += value;
+                        break;
+                    case "down":
+                        depth += value;
+                        break;
+                    case "up":
+                        depth -= value;
+                        break;
+                }
+            }
+
+            Console.WriteLine("Part 1: " + horizontal * depth);
+
+
+            horizontal = 0;
+            depth = 0;
+            int aim = 0;
+
+            //Part 2
+
+            foreach (var input in inputs) {
+                string direction = input.Split(' ')[0];
+                int value = int.Parse(input.Split(' ')[1]);
+
+                switch (direction) {
+                    case "forward":
+                        horizontal += value;
+                        depth += value * aim;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                }
+            }
+
+            Console.WriteLine("Part 2: " + horizontal * depth);
+
+        }
+    }
+}
